1939c7a095129adb3805e5b1ad4593259f01964b
zhangcong <zhangcong@cgwic.xyz>
2023-07-24 15:32:18 +0800

    优化了截图清晰度和按钮样式

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ class screenRs {
         this.videoReplay = null
         this.input = null
         this.cur_doing = ''
+        this.curBtn = ''
         this.positions = {
             x: 0,
             y: 0
@@ -23,6 +24,7 @@ class screenRs {
         this.canvasHistory = []
         this.init()
         this.dragging = false
+        this.scale = 1
     }
     init() {
         //文字输入框
@@ -45,6 +47,7 @@ class screenRs {
         this.container.style.display = 'none';
         this.container.style.justifyContent = 'center';
         this.container.style.alignItems = 'center';
+        this.container.style.overflow = 'auto';
         //创建canvas标签，用于截图显示和下载
         this.canvas = document.createElement('canvas');
         //创建video标签，用于视频回放
@@ -58,6 +61,7 @@ class screenRs {
         this.toolDiv.style.top = '10px';
         //功能按钮
         let shotBtn = document.createElement('button')
+        shotBtn.classList.add('ccc-btn')
         shotBtn.style.marginLeft = "10px"
         let svg_shot = `<div style="display: flex;justify-content: space-between;align-items: center">
         <svg t="1689901856063" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="currentColor" p-id="1491" width="16" height="16"><path d="M715.98 634.01h80.07V308.02c0-21.61-7.94-40.35-23.83-56.24-15.89-15.89-34.63-23.83-56.24-23.83H389.99v80.07h325.99v325.99z m-407.96 81.97V64h-80.07v163.95H64v80.07h163.95v407.97c0 21.6 7.94 40.35 23.83 56.24 15.89 15.89 34.63 23.83 56.24 23.83h407.97V960h80.07V796.05H960v-80.07H308.02z" p-id="1492"></path></svg>
@@ -65,9 +69,12 @@ class screenRs {
         </div>`
         shotBtn.innerHTML = svg_shot
         shotBtn.onclick = () => {
+            this.removeBtnStatus()
+            shotBtn.classList.add('active-btn')
             this.strokeShotRect()
         }
         let penBtn = document.createElement('button')
+        penBtn.classList.add('ccc-btn')
         penBtn.style.marginLeft = "10px"
         let svg_pen = `<div style="display: flex;justify-content: space-between;align-items: center">
         <svg t="1689215698932" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="currentColor" p-id="7206" width="16" height="16"><path d="M895.855 313.163l-78.107 78.107-185.04-185.041 78.07-78.107c26.508-26.51 53.017 0 53.017 0l132.06 132.022c0 0.001 26.509 26.51 0 53.019zM116.359 907.654l224.337-39.37-185.452-185.452-38.885 224.822z m64.902-249.993l185.066 185.066 423.009-423.009L604.27 234.652l-423.01 423.01z" p-id="7207"></path></svg>
@@ -75,9 +82,12 @@ class screenRs {
         </div>`
         penBtn.innerHTML = svg_pen
         penBtn.onclick = () => {
+            this.removeBtnStatus()
+            penBtn.classList.add('active-btn')
             this.strokeLine()
         }
         let rectBtn = document.createElement('button')
+        rectBtn.classList.add('ccc-btn')
         rectBtn.style.marginLeft = "10px"
         let svg_rect = `<div style="display: flex;justify-content: space-between;align-items: center">
         <svg t="1689215650398" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="currentColor" p-id="5871" width="16" height="16"><path d="M898.8 199.6v624.8H125.2V199.6h773.6m59.5-59.5H65.7v743.8h892.5V140.1h0.1z" p-id="5872"></path></svg>
@@ -85,9 +95,12 @@ class screenRs {
         </div>`
         rectBtn.innerHTML = svg_rect
         rectBtn.onclick = () => {
+            this.removeBtnStatus()
+            rectBtn.classList.add('active-btn')
             this.strokeRect()
         }
         let wordBtn = document.createElement('button')
+        wordBtn.classList.add('ccc-btn')
         wordBtn.style.marginLeft = "10px"
         let svg_word = `<div style="display: flex;justify-content: space-between;align-items: center">
         <svg t="1689215577593" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="currentColor" p-id="4839" width="16" height="16"><path d="M429.056 919.552h166.4l-0.512-1.024c-32.768-40.96-50.688-92.16-50.688-144.384V228.352h154.112c44.032 0 87.04 14.848 121.856 42.496l11.776 9.216V154.112s-128 20.48-319.488 20.48-321.024-20.48-321.024-20.48v125.44l8.192-6.656c35.328-28.672 79.36-44.544 124.928-44.544h155.648v545.28c0 52.736-17.92 103.424-50.688 144.384l-0.512 1.536z" fill="currentColor" p-id="4840"></path></svg>
@@ -95,9 +108,12 @@ class screenRs {
         </div>`
         wordBtn.innerHTML = svg_word
         wordBtn.onclick = () => {
+            this.removeBtnStatus()
+            wordBtn.classList.add('active-btn')
             this.strokeWord()
         }
         let backBtn = document.createElement('button')
+        backBtn.classList.add('ccc-btn')
         backBtn.style.marginLeft = "10px"
         let svg_back = `<div style="display: flex;justify-content: space-between;align-items: center">
         <svg t="1689215549677" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="currentColor" p-id="3691" width="16" height="16"><path d="M704 320 231.552 320l141.504-141.376c12.032-11.968 12.032-31.36 0-43.328-12.032-12.032-31.424-12.032-43.456 0L135.296 329.344c-12.032 11.968-12.032 31.36 0 43.392L329.6 566.784c12.032 11.968 31.424 11.968 43.456 0 12.032-12.032 12.032-31.36 0-43.392L233.408 384 704 384c106.048 0 192 85.952 192 192s-85.952 192-192 192L288 768C270.336 768 256 782.336 256 800S270.336 832 288 832L704 832c141.376 0 256-114.624 256-256S845.376 320 704 320z" p-id="3692"></path></svg>
@@ -108,6 +124,7 @@ class screenRs {
             this.goBack()
         }
         let downloadBtn = document.createElement('button')
+        downloadBtn.classList.add('ccc-btn')
         downloadBtn.style.marginLeft = "10px"
         let svg_downlog = `<div style="display: flex;justify-content: space-between;align-items: center">
         <svg t="1689214502963" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="currentColor" p-id="1486" width="16" height="16"><path d="M328 576h152V128h64v448h152L512 768 328 576z m568-64h-64v320H192V512h-64v384h768V512z" p-id="1487"></path></svg>
@@ -122,6 +139,7 @@ class screenRs {
             }
         }
         let closeBtn = document.createElement('button')
+        closeBtn.classList.add('ccc-btn')
         closeBtn.style.marginLeft = "10px"
         let svg_close = `<div style="display: flex;justify-content: space-between;align-items: center">
         <svg t="1689215472725" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="currentColor" p-id="2645" width="16" height="16"><path d="M512 128C300.8 128 128 300.8 128 512s172.8 384 384 384 384-172.8 384-384S723.2 128 512 128zM512 832c-179.2 0-320-140.8-320-320s140.8-320 320-320 320 140.8 320 320S691.2 832 512 832z" p-id="2646"></path><path d="M672 352c-12.8-12.8-32-12.8-44.8 0L512 467.2 396.8 352C384 339.2 364.8 339.2 352 352S339.2 384 352 396.8L467.2 512 352 627.2c-12.8 12.8-12.8 32 0 44.8s32 12.8 44.8 0L512 556.8l115.2 115.2c12.8 12.8 32 12.8 44.8 0s12.8-32 0-44.8L556.8 512l115.2-115.2C684.8 384 684.8 364.8 672 352z" p-id="2647"></path></svg>
@@ -137,6 +155,8 @@ class screenRs {
             }
             this.container.style.display = 'none'
             this.cur_doing = ''
+            this.removeBtnStatus()
+            this.offlisten()
         }
         this.toolShot = document.createElement('div')
         this.toolShot.appendChild(shotBtn)
@@ -157,6 +177,18 @@ class screenRs {
         return new Promise((resolve, reject) => {
             navigator.mediaDevices.getDisplayMedia({ video: true, audio: false }).then(stream => {
                 this.displayStream = stream;
+                // 设置视频分辨率
+                // const videoTrack = stream.getVideoTracks()[0];
+                // const constraints = { width: 1920, height: 1080 };
+                // videoTrack.applyConstraints(constraints)
+                //   .then(function() {
+                //     // 分辨率设置成功
+                //     console.log('分辨率设置成功')
+                //   })
+                //   .catch(function(error) {
+                //     // 分辨率设置失败
+                //     console.log('分辨率设置失败')
+                //   });
                 this.mediaRecorder = new MediaRecorder(this.displayStream, this.options);
                 this.mediaRecorder.ondataavailable = event => {
                     if (event.data.size > 0) {
@@ -172,7 +204,6 @@ class screenRs {
     async shot() {
         this.cur_doing = 'shot'
         this.toolShot.style.display = 'inline-block'
-        document.body.style.cursor = 'none'
         if (!this.displayStream) await this.getDisplayMedia()
         const video = document.createElement('video');
         video.classList.add('ccc-video');
@@ -180,22 +211,25 @@ class screenRs {
         video.controls = false;
         const canvas = this.canvas
         const ctx = this.ctx = canvas.getContext("2d");
+        let that = this
         video.addEventListener('canplay', function () {
             let canvasHeight = window.innerHeight * 0.9;
             let canvasWidth = window.innerHeight * 0.9 * this.videoWidth / this.videoHeight;
-            canvas.setAttribute('width', canvasWidth)
-            canvas.setAttribute('height', canvasHeight)
-            // ctx.scale(canvasWidth / this.videoWidth, canvasHeight / this.videoHeight);
+            let ratio = window.devicePixelRatio;
+            that.scale = this.videoHeight / canvasHeight
+            canvas.setAttribute('width', this.videoWidth * ratio)
+            canvas.setAttribute('height', this.videoHeight * ratio)
+            canvas.style.width = canvasWidth + "px";
+            canvas.style.height = canvasHeight + "px";
+            ctx.scale(ratio, ratio);
         });
         // 获取Canvas元素
         video.play();
+        document.body.style.cursor = 'none'
         setTimeout(() => {
             // 创建一个2D绘图上下文
             ctx.drawImage(video, 0, 0);
             document.body.style.cursor = ''
-            // var pattern = ctx.createPattern(canvas, "no-repeat");
-            // ctx.fillStyle = pattern;
-            // ctx.fillRect(0, 0, canvas.width, canvas.height);
             video.pause();
             this.stop()
             this.showContainer()
@@ -207,6 +241,7 @@ class screenRs {
     }
     async strokeWord() {
         await this.offlisten();
+        this.curBtn = 'word';
         // this.ctx.fillText('Hello world', 10, 50)
         const textBox = this.input
         const that = this;
@@ -219,10 +254,11 @@ class screenRs {
                 that.ctx.lineWidth = 5; // 指定描边线的宽度
                 var rect = that.input.getBoundingClientRect();
                 var max_width = that.canvas.getBoundingClientRect().width - rect.width
+                console.log(max_width)
                 that.ctx.fillText(
                     textBox.innerText,
-                    that.positions.x < max_width ? that.positions.x : max_width,
-                    that.positions.y < 20 ? 20 : that.positions.y
+                    that.positions.x * that.scale < max_width ? that.positions.x * that.scale : max_width,
+                    that.positions.y * that.scale < 20 ? 20 : that.positions.y * that.scale
                 );
                 textBox.style.display = "none";
                 textBox.innerText = "";
@@ -239,46 +275,9 @@ class screenRs {
             });
         };
     }
-    async fillY() {
-        await this.offlisten();
-        this.clear();
-        let postx = {
-            startX: 0,
-            endX: 0,
-            startY: 0,
-            endY: 0,
-        };
-        const that = this;
-        this.canvas.onmousedown = function mouseDownAction(e) {
-            postx.startX = e.offsetX;
-            postx.startY = e.offsetY;
-            that.addHistory();
-        };
-        this.canvas.onmouseup = function mouseUp(e) {
-            postx.endX = e.offsetX;
-            postx.endY = e.offsetY;
-            that.ctx.fillStyle = "red"; // 填充颜色为红色
-            that.ctx.strokeStyle = that.color; // 画笔的颜色
-            that.ctx.lineWidth = 5; // 指定描边线的宽度
-            that.ctx.beginPath();
-            const length = Math.sqrt(
-                Math.pow(postx.startX - postx.endX, 2) +
-                Math.pow(postx.startY - postx.endY, 2)
-            );
-
-            that.ctx.arc(postx.startX, postx.startY, length, 0, 2 * Math.PI);
-            that.ctx.stroke();
-            that.ctx.closePath();
-            postx = {
-                startX: 0,
-                endX: 0,
-                startY: 0,
-                endY: 0,
-            };
-        };
-    }
     async strokeRect() {
         await this.offlisten();
+        this.curBtn = 'rect';
         this.clear();
         let postx = {
             startX: 0,
@@ -295,39 +294,27 @@ class screenRs {
             that.ctx.strokeStyle = that.color; // 画笔的颜色
             that.ctx.lineWidth = 5; // 指定描边线的宽度
             that.dragging = true
+            console.log(that.scale)
         };
         this.canvas.onmousemove = function mouseMove(event) {
             if (!that.dragging) return
             that.ctx.putImageData(that.canvasHistory[that.canvasHistory.length - 1].data, 0, 0);
             that.ctx.beginPath();
             that.ctx.strokeRect(
-                postx.startX,
-                postx.startY,
-                event.offsetX - postx.startX,
-                event.offsetY - postx.startY
+                postx.startX * that.scale,
+                postx.startY * that.scale,
+                (event.offsetX - postx.startX) * that.scale,
+                (event.offsetY - postx.startY) * that.scale
             );
         }
         this.canvas.onmouseup = function mouseUp(e) {
             that.dragging = false
-            postx.endX = e.offsetX;
-            postx.endY = e.offsetY;
-            // that.ctx.fillStyle = "red"; // 填充颜色为红色
-            // that.ctx.strokeStyle = that.color; // 画笔的颜色
-            // that.ctx.lineWidth = 5; // 指定描边线的宽度
-
-            that.ctx.beginPath();
-            that.ctx.strokeRect(
-                postx.startX,
-                postx.startY,
-                postx.endX - postx.startX,
-                postx.endY - postx.startY
-            );
-
             that.ctx.closePath();
         };
     }
     async strokeLine() {
         await this.offlisten();
+        this.curBtn = 'line';
         const that = this;
         this.ctx.fillStyle = "#FF0000";
         this.ctx.lineWidth = 5;
@@ -347,8 +334,8 @@ class screenRs {
                 that.ctx.globalAlpha = 1;
                 that.ctx.lineWidth = 5;
                 that.ctx.strokeStyle = that.color;
-                that.ctx.moveTo(x, y);
-                that.ctx.lineTo(x1, y1);
+                that.ctx.moveTo(x * that.scale, y * that.scale);
+                that.ctx.lineTo(x1 * that.scale, y1 * that.scale);
                 that.ctx.closePath();
                 that.ctx.stroke();
                 x = x1;
@@ -363,6 +350,7 @@ class screenRs {
     async strokeShotRect() {
         await this.offlisten();
         this.clear();
+        this.curBtn = 'shotRect';
         let postx = {
             startX: 0,
             endX: 0,
@@ -385,35 +373,49 @@ class screenRs {
             that.ctx.putImageData(that.canvasHistory[that.canvasHistory.length - 1].data, 0, 0);
             that.ctx.beginPath();
             that.ctx.strokeRect(
-                postx.startX,
-                postx.startY,
-                event.offsetX - postx.startX,
-                event.offsetY - postx.startY
+                postx.startX * that.scale,
+                postx.startY * that.scale,
+                (event.offsetX - postx.startX) * that.scale,
+                (event.offsetY - postx.startY) * that.scale
             );
         }
         this.canvas.onmouseup = function mouseUp(e) {
             that.dragging = false
             that.ctx.closePath();
-            let endX = e.offsetX - postx.startX;
-            let endY = e.offsetY - postx.startY;
             that.ctx.putImageData(that.canvasHistory[that.canvasHistory.length - 1].data, 0, 0);
-            var data = that.ctx.getImageData(postx.startX, postx.startY, endX, endY);
-            that.ctx.clearRect(0, 0, that.canvas.width, that.canvas.height);
+            let ratio = window.devicePixelRatio;
+            let startX = postx.startX * that.scale * ratio;
+            let startY = postx.startY * that.scale * ratio;
+            let endX = Math.abs((e.offsetX - postx.startX) * that.scale * ratio);
+            let endY = Math.abs((e.offsetY - postx.startY) * that.scale * ratio);
+            if (e.offsetX < postx.startX) {
+                startX = e.offsetX * that.scale * ratio;
+                startY = e.offsetY * that.scale * ratio;
+            }
+            var data = that.ctx.getImageData(startX, startY, endX, endY);
+            that.ctx.clearRect(0, 0, that.canvas.width / ratio, that.canvas.height / ratio);
             that.canvas.width = endX;
             that.canvas.height = endY;
             that.ctx.putImageData(data, 0, 0);
+            that.canvas.style.width = endX / ratio / that.scale + "px";
+            that.canvas.style.height = endY / ratio / that.scale + "px";
+            that.ctx.scale(ratio, ratio);
         };
     }
     async goBack() {
         await this.clear();
-        await this.offlisten();
+        // await this.offlisten();
         if (this.canvasHistory.length) {
             let that = this;
             let last = this.canvasHistory.pop()
             let size = last.size
+            let cssSize = last.cssSize
             let canvasPic = last.data
             that.canvas.width = size[0];
             that.canvas.height = size[1];
+            that.canvas.style.width = cssSize[0];
+            that.canvas.style.height = cssSize[1];
+            that.ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
             that.ctx.putImageData(canvasPic, 0, 0);
         } else {
             alert("不能再撤回了");
@@ -423,10 +425,18 @@ class screenRs {
         // 去掉所有绘制
         this.input.style.display = "none";
     }
+    removeBtnStatus() {
+        var btns = this.toolShot.childNodes
+        Array.from(btns).forEach(btn => {
+            btn.classList.remove('active-btn')
+        })
+        this.curBtn = ''
+    }
     addHistory() {
         this.canvasHistory.push(
             {
                 size: [this.canvas.width, this.canvas.height],
+                cssSize: [this.canvas.style.width, this.canvas.style.height],
                 data: this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height)
             }
         );
